80e2e94eac788dc1435982818b2b24c3c9df25d6
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-16 23:21:24 +0900

    added keycode detection

---
+++ index.js
@@ -0,0 +1,22 @@
+const pressed = [];
+const secretCode = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+
+window.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join("") === secretCode.join("")) {
+    console.log("hooray");
+  }
+});
